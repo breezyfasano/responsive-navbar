4a21c9ceba307d5f776a875e021b2f2fb95beb4a
Breezy Fasano <breezyfasano@breezys-mbp.attlocal.net>
2018-10-29 01:04:55 -0700

    mobile styling finished

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -1,23 +1,34 @@
 'use strict';
 const navbar = document.getElementById('nav');
-const navbarChildren = navbar.children;
 const openIcon = document.querySelector('.open-icon');
 const closeIcon = document.querySelector('.close-icon');
 
-/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
-function openNav() {
+const openNav = () => {
+    let navbarChildren = navbar.children;
     for (let i = 0; i < 2; i++) {
-        navbar.style.width = "250px";
-        navbarChildren[i].style.width = "250px";
+        let widthValue = '55vw';
+        navbar.style.width = widthValue;
+        navbarChildren[i].style.width = widthValue;
     }
-    document.querySelector('main').style.marginLeft = "250px";
-}
+};
 
-/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
-function closeNav() {
+const closeNav = () => {
     for (let i = 0; i < 2; i++) {
         navbar.style.width = 0;
         navbarChildren[i].style.width = 0;
     }
     document.querySelector('main').style.marginLeft = 0;
-}
+};
+
+
+openIcon.addEventListener ('click',
+ function() {
+        openNav();
+    }
+ );
+
+ closeIcon.addEventListener('click',
+     function () {
+         closeNav();
+     }
+ );
